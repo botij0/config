9ba3ae6f0045900489d88dc1e4317adf32e56f31
Javier González Soldado <95412468+botij0@users.noreply.github.com>
2025-04-17 09:58:31 +0200

    feat: nerd font  .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,16 +2,16 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.default_prog = {'wsl', '-d', 'Ubuntu', '--cd', '~' }
+config.default_prog = {"/usr/bin/zsh"}
 
 config.font_size = 12
 config.line_height = 1.2
-config.font = wezterm.font('Cascadia Code')
+config.font = wezterm.font('CaskaydiaCove Nerd Font')
 
 config.font_rules = {
     {
       italic = true,
-      font = wezterm.font('Cascadia Code', {
+      font = wezterm.font('CaskaydiaCove Nerd Font', {
         italic = true,
       }),
     },
@@ -25,6 +25,7 @@ config.macos_window_background_blur = 40
 
 config.max_fps = 120
 config.prefer_egl = true
+
 
 config.initial_rows=23
 config.initial_cols=125
